0c4192423e15e143021b39534c5a11f2a462f434
JesseColon <jay.alexandervii@gmail.com>
2023-08-08 19:52:09 -0400

    trying to use bodyParser

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 // server.js
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const path = require('path');
-const routes = require('./routes');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve the static frontend files
 app.use(express.static(path.join(__dirname, 'public')));
